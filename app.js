e426a52cedbc526ce81c5e09de015812d8a2db4e
Diana Gouveia <dianacpgouveia@gmail.com>
2021-03-04 17:24:01 +0000

    var for let

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
-var numberSquares = 6;
-var colors = randomColors(6);
-var h1 = document.querySelector("h1");
-var squares = document.querySelectorAll(".square");
-var colorPicked = pickColor();
-var winningColor = document.querySelector("#winningColor");
+let numberSquares = 6;
+let colors = randomColors(6);
+let squares = document.querySelectorAll(".square");
+let colorPicked = pickColor();
+let winningColor = document.querySelector("#winningColor");
 winningColor.textContent = colorPicked;
-var message = document.querySelector("#message");
-var h1 = document.querySelector("h1");
-var easyButton = document.querySelector("#easymode");
-var hardButton = document.querySelector("#hardmode");
+let message = document.querySelector("#message");
+let h1 = document.querySelector("h1");
+let easyButton = document.querySelector("#easymode");
+let hardButton = document.querySelector("#hardmode");
 
 // Response when clicking squares
-for (var i = 0; i < squares.length; i++) {
+for (let i = 0; i < squares.length; i++) {
   squares[i].style.backgroundColor = colors[i];
   squares[i].addEventListener("click", function () {
-    var clickedColor = this.style.backgroundColor;
+    let clickedColor = this.style.backgroundColor;
     if (clickedColor === colorPicked) {
       message.textContent = "Correct!";
       h1.style.backgroundColor = clickedColor;
@@ -32,21 +31,21 @@ for (var i = 0; i < squares.length; i++) {
 // loop colors
 function changeColors(color) {
   h1.style.backgroundColor = color;
-  for (var i = 0; i < squares.length; i++) {
+  for (let i = 0; i < squares.length; i++) {
     squares[i].style.backgroundColor = color;
   }
 }
 
-// Pick new winner colour by selecting the index array
+// Pick new winner color by selecting the index array
 function pickColor() {
-  var indexwinner = Math.floor(Math.random() * colors.length);
+  let indexwinner = Math.floor(Math.random() * colors.length);
   return colors[indexwinner];
 }
 
 // Creating an array with colors
 function randomColors(amount) {
-  var array = [];
-  for (var i = 0; i < amount; i++) {
+  let array = [];
+  for (let i = 0; i < amount; i++) {
     array.push(randomColor());
   }
   return array;
@@ -54,10 +53,10 @@ function randomColors(amount) {
 
 // Creating RGB color code RGB (0-255, 0-255, 0-255)
 function randomColor() {
-  var red = Math.floor(Math.random() * 256);
-  var green = Math.floor(Math.random() * 256);
-  var blue = Math.floor(Math.random() * 256);
-  var color = "rgb(" + red + ", " + green + ", " + blue + ")";
+  let red = Math.floor(Math.random() * 256);
+  let green = Math.floor(Math.random() * 256);
+  let blue = Math.floor(Math.random() * 256);
+  let color = "rgb(" + red + ", " + green + ", " + blue + ")";
   return color;
 }
 
@@ -69,7 +68,7 @@ easyButton.addEventListener("click", function () {
   colors = randomColors(numberSquares);
   colorPicked = pickColor();
   winningColor.textContent = colorPicked;
-  for (var i = 0; i < squares.length; i++) {
+  for (let i = 0; i < squares.length; i++) {
     squares[i].style.backgroundColor = colors[i];
     if (i > 2) {
       squares[i].style.display = "none";
@@ -85,7 +84,7 @@ hardButton.addEventListener("click", function () {
   colors = randomColors(numberSquares);
   colorPicked = pickColor();
   winningColor.textContent = colorPicked;
-  for (var i = 0; i < squares.length; i++) {
+  for (let i = 0; i < squares.length; i++) {
     squares[i].style.backgroundColor = colors[i];
     if (squares[i].style.display === "none") {
       squares[i].style.display = "block";
@@ -99,7 +98,7 @@ reset.addEventListener("click", function () {
   colors = randomColors(numberSquares);
   colorPicked = pickColor();
   winningColor.textContent = colorPicked;
-  for (var i = 0; i < colors.length; i++) {
+  for (let i = 0; i < colors.length; i++) {
     squares[i].style.backgroundColor = colors[i];
   }
   h1.style.backgroundColor = "#232323";
